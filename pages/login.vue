851cde219c7a697eee60dd039f87b52f162a0728
Andrias Melianus Siwabessy <andriasmelianus@gmail.com>
2021-04-12 10:14:02 +0700

    Utilize default layout to login page.

--- pages/login.vue
+++ pages/login.vue
@@ -1,69 +1,66 @@
 <template>
-  <v-app>
-    <v-main>
-      <v-container class="fill-height" fluid>
-        <v-row align="center" justify="center">
-          <v-col cols="12" sm="8" md="4">
-            <v-card class="elevation-12">
-              <v-toolbar color="primary" dark flat>
-                <v-toolbar-title
-                  >Login - {{ this.APPLICATION_NAME }}</v-toolbar-title
-                >
-                <v-spacer></v-spacer>
-              </v-toolbar>
+  <v-container class="fill-height" fluid>
+    <v-row align="center" justify="center">
+      <v-col cols="12" sm="8" md="4">
+        <v-card class="elevation-12">
+          <v-toolbar color="primary" dark flat>
+            <v-toolbar-title
+              >Login - {{ this.APPLICATION_NAME }}</v-toolbar-title
+            >
+            <v-spacer></v-spacer>
+          </v-toolbar>
 
-              <v-form @submit.prevent="submitLogin">
-                <v-card-text>
-                  <v-text-field
-                    v-model="email"
-                    :error-messages="invalidInputMessage.email[0]"
-                    label="Email"
-                    name="email"
-                    prepend-icon="mdi-account-lock"
-                    type="text"
-                  ></v-text-field>
+          <v-form @submit.prevent="submitLogin">
+            <v-card-text>
+              <v-text-field
+                v-model="email"
+                :error-messages="invalidInputMessage.email[0]"
+                label="Email"
+                name="email"
+                prepend-icon="mdi-account-lock"
+                type="text"
+              ></v-text-field>
 
-                  <v-text-field
-                    v-model="password"
-                    :error-messages="invalidInputMessage.password[0]"
-                    label="Password"
-                    name="password"
-                    prepend-icon="mdi-lock"
-                    type="password"
-                  ></v-text-field>
+              <v-text-field
+                v-model="password"
+                :error-messages="invalidInputMessage.password[0]"
+                label="Password"
+                name="password"
+                prepend-icon="mdi-lock"
+                type="password"
+              ></v-text-field>
 
-                  <v-checkbox
-                    v-model="remember"
-                    label="Ingat saya"
-                    color="primary"
-                  ></v-checkbox>
-                </v-card-text>
+              <v-checkbox
+                v-model="remember"
+                label="Ingat saya"
+                color="primary"
+              ></v-checkbox>
+            </v-card-text>
 
-                <v-card-actions>
-                  <v-btn color="primary" type="submit">Masuk</v-btn>
-                  <v-spacer></v-spacer>
-                  <v-btn color="default" v-show="false">
-                    <v-icon>mdi-account-circle</v-icon>&nbsp; Daftar...
-                  </v-btn>
-                </v-card-actions>
-              </v-form>
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-main>
+            <v-card-actions>
+              <v-btn color="primary" type="submit">Masuk</v-btn>
+              <v-spacer></v-spacer>
+              <v-btn color="default" v-show="false">
+                <v-icon>mdi-account-circle</v-icon>&nbsp; Daftar...
+              </v-btn>
+            </v-card-actions>
+          </v-form>
+        </v-card>
+      </v-col>
+    </v-row>
 
     <v-snackbar v-model="loginFail" color="error" :top="true">
       Login gagal
       <br />
       {{ errorMessage }}
     </v-snackbar>
-  </v-app>
+  </v-container>
 </template>
 
 <script>
 import { MessageHandler } from "~/components/_mixins/message-handler";
 export default {
+  layout: "default",
   auth: "guest",
   head() {
     return {
