c1db7dd3c1a5d30dcf275a09e0a11acaaa5aee2a
Andrias Melianus Siwabessy <andriasmelianus@gmail.com>
2021-04-26 23:20:28 +0700

    Add branch specific page (dynamic routing).

---
+++ pages/branch/_id.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-row class="fill-height">
+    <v-col cols="12" md="6">
+      <v-card>
+        <branch-form
+          :record="branchData"
+          edit-mode
+          @recordUpdated="branchUpdated"
+        ></branch-form>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import { MessageExtractor } from "~/components/_mixins/message-extractor";
+import BranchForm from "~/components/branch/_forms/default";
+export default {
+  layout: "dashboard",
+
+  mixins: [MessageExtractor],
+
+  components: {
+    BranchForm,
+  },
+
+  mounted() {
+    let vm = this;
+    vm.$axios
+      .$get("/branch/" + vm.branchId)
+      .then(function (result) {
+        vm.branchData = Object.assign({}, vm.branchData, result);
+      })
+      .catch(function (result) {
+        console.log(result);
+      });
+  },
+
+  data: () => ({
+    branchData: {},
+  }),
+
+  computed: {
+    branchId() {
+      return this.$route.params.id;
+    },
+  },
+
+  methods: {
+    branchUpdated(result) {
+      let vm = this;
+      vm.$store.commit("global-snackbar/show", {
+        message: vm.messageSuccessExtract(result),
+        color: "success",
+      });
+    },
+  },
+};
+</script>
