b4ab883e6c60ae5279f32dfebd56696fa452221b
Andrias Melianus Siwabessy <andriasmelianus@gmail.com>
2021-04-27 11:22:24 +0700

    Add branch table component.

---
+++ components/branch/_tables/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-data-table
+    :headers="headers"
+    :items="records"
+    show-select
+    single-select
+    v-model="selectedRecords"
+    :search="search"
+    @item-selected="handleRecordSelected"
+  ></v-data-table>
+</template>
+
+<script>
+import { Handler } from "~/components/branch/_tables/handler";
+export default {
+  mixins: [Handler],
+};
+</script>
---
+++ components/branch/_tables/handler.js
@@ -0,0 +1,26 @@
+import { GlobalDataTableHandler } from "~/components/_mixins/global-data-table-handler";
+import { DefaultRecord } from '~/components/branch/record'
+export const Handler = {
+    mixins: [GlobalDataTableHandler],
+
+    data: () => ({
+        headers: [
+            { text: "Nama", value: "name", align: "left" },
+            { text: "Deskripsi", value: "description" },
+            { text: "NPWP", value: "tax_id" },
+            { text: "Bisnis", value: "business" },
+            { text: "Industri", value: "industry" },
+            { text: "Web", value: "website" },
+            { text: "Catatan", value: "note" },
+            { text: "Diinput", value: "created_at" },
+            { text: "Diupdate", value: "updated_at" }
+        ],
+        smallHeaders: [{ text: "Nama", value: "name", align: "left" }],
+        singleColumnHeader: [{ text: "Nama", value: "name", align: "left" }],
+
+        defaultRecord: DefaultRecord,
+        editedRecord: DefaultRecord,
+
+        resourceUri: "/branch"
+    })
+}
