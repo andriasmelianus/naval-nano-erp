2b1193fc0cc8bd527aeee6540bbfddcb255d9aac
Andrias Melianus Siwabessy <andriasmelianus@gmail.com>
2021-05-14 16:36:04 +0700

    Add branch entity tab component.

---
+++ components/branch/entity-tabs.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-card>
+    <v-tabs vertical>
+      <v-tab>
+        <v-icon left>mdi-account</v-icon>
+        Pengguna
+      </v-tab>
+      <v-tab>
+        <v-icon left>mdi-page-next</v-icon>
+        Ruangan
+      </v-tab>
+      <v-tab>
+        <v-icon left>mdi-broom</v-icon>
+        Produk
+      </v-tab>
+
+      <v-tab-item>
+        <v-card flat>
+          <v-card-text
+            >Pengguna <br />Lorem ipsum dolor sit amet consectetur adipisicing
+            elit. Id enim voluptas alias vero nostrum, voluptatum molestiae quo
+            odio at officia et, quisquam repellendus, beatae explicabo aut!
+            Culpa iste nihil sunt.</v-card-text
+          >
+        </v-card>
+      </v-tab-item>
+      <v-tab-item>
+        <v-card flat>
+          <v-card-text
+            >Ruangan <br />Lorem ipsum dolor, sit amet consectetur adipisicing
+            elit. Magni voluptatum, voluptas fugit beatae temporibus impedit
+            excepturi provident esse ab quasi! Amet, fuga suscipit? Autem nemo
+            facilis cumque provident numquam asperiores.</v-card-text
+          >
+        </v-card>
+      </v-tab-item>
+      <v-tab-item>
+        <v-card flat>
+          <v-card-text
+            >Produk <br />Lorem ipsum dolor sit amet consectetur adipisicing
+            elit. Ipsum, dolorum sequi! Saepe, ut, explicabo pariatur fuga, unde
+            placeat a et ullam laborum architecto maxime optio veniam inventore.
+            Vero, a ex.</v-card-text
+          >
+        </v-card>
+      </v-tab-item>
+    </v-tabs>
+  </v-card>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
