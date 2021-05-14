ee2d686eae1b9892de4009814a4f84f1a46420a0
Andrias Melianus Siwabessy <andriasmelianus@gmail.com>
2021-05-14 17:51:44 +0700

    Add user default and default small data table.

---
+++ components/user/_tables/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-data-table
+    :headers="headers"
+    :items="records"
+    :server-items-length="recordsTotal"
+    :options.sync="serverParams"
+    :loading="isLoading"
+    show-select
+    single-select
+    v-model="selectedRecords"
+    :search="searchKeywords"
+    @item-selected="handleRecordSelected"
+  ></v-data-table>
+</template>
+
+<script>
+import { Handler } from "./handler";
+export default {
+  mixins: [Handler],
+};
+</script>
---
+++ components/user/_tables/handler.js
@@ -0,0 +1,28 @@
+import { GlobalServerSideDataTableHandler } from '~/components/_mixins/global-server-side-data-table-handler'
+import { DefaultRecord } from '~/components/user/record'
+export const Handler = {
+    mixins: [GlobalServerSideDataTableHandler],
+
+    data: () => ({
+        headers: [
+            { text: "Nama", value: "name", align: "left" },
+            { text: "Email", value: "email" },
+            { text: "Telepon", value: "phone" },
+            { text: "Alamat", value: "address" },
+            { text: "Kota", value: "city" },
+            { text: "Negara", value: "country" },
+            { text: "Kode Pos", value: "postal_code" },
+            { text: "Diupdate", value: "updated_at" },
+        ],
+        smallHeaders: [
+            { text: "Nama", value: "name", align: "left" },
+            { text: "Email", value: "email" },
+        ],
+        singleColumnHeader: [{ text: "Nama", value: "name", align: "left" }],
+
+        defaultRecord: DefaultRecord,
+        editedRecord: DefaultRecord,
+
+        resourceUri: '/user'
+    }),
+}
