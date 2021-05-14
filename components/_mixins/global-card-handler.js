8b0411d9ab74581780b1fe2f026e7dbcb8ba6382
Andrias Melianus Siwabessy <andriasmelianus@gmail.com>
2021-05-14 13:18:56 +0700

    Add title props to set card title.

--- components/_mixins/global-card-handler.js
+++ components/_mixins/global-card-handler.js
@@ -1,5 +1,11 @@
 export const GlobalCardHandler = {
     props: {
+        // The card title.
+        title: {
+            type: String,
+            default: ''
+        },
+
         // Record to be displayed in the card.
         record: {
             type: Object,
