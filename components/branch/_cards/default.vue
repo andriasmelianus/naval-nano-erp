4c2b23bfc834e0fc2ff8f35b5b47263a0388437c
Andrias Melianus Siwabessy <andriasmelianus@gmail.com>
2021-05-14 13:19:59 +0700

    Add branch default card.

---
+++ components/branch/_cards/default.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-card max-width="400">
+    <v-app-bar flat dense>
+      <v-toolbar-title>{{ title }}</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon>
+        <v-icon>mdi-dots-vertical</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-list-item>
+      <v-list-item-subtitle>Nama Cabang</v-list-item-subtitle>
+      <v-list-item-title>{{ record.name }}</v-list-item-title>
+    </v-list-item>
+
+    <v-list-item dense>
+      <v-list-item-subtitle>Kode Cabang</v-list-item-subtitle>
+      <v-list-item-title>{{ record.code }}</v-list-item-title>
+    </v-list-item>
+
+    <v-list-item dense>
+      <v-list-item-subtitle>Telepon</v-list-item-subtitle>
+      <v-list-item-title>{{ record.phone }}</v-list-item-title>
+    </v-list-item>
+
+    <v-list-item dense>
+      <v-list-item-subtitle>Alamat</v-list-item-subtitle>
+      <v-list-item-title>{{ record.address }}</v-list-item-title>
+    </v-list-item>
+
+    <v-list-item dense>
+      <v-list-item-subtitle>Kota</v-list-item-subtitle>
+      <v-list-item-title>{{ record.city }}</v-list-item-title>
+    </v-list-item>
+
+    <v-list-item dense>
+      <v-list-item-subtitle>Negara</v-list-item-subtitle>
+      <v-list-item-title>{{ record.country }}</v-list-item-title>
+    </v-list-item>
+
+    <v-list-item dense>
+      <v-list-item-subtitle>Kode Pos</v-list-item-subtitle>
+      <v-list-item-title>{{ record.postal_code }}</v-list-item-title>
+    </v-list-item>
+
+    <v-divider></v-divider>
+
+    <v-card-actions>
+      <v-btn text nuxt :to="'/branch/' + record.id">Edit</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+import { Handler } from "./handler";
+export default {
+  mixins: [Handler],
+};
+</script>
