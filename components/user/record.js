b6cec966905d6058bb090496f3b0cb65f1b3bb92
Andrias Melianus Siwabessy <andriasmelianus@gmail.com>
2021-05-14 17:51:16 +0700

    Add user default record and default invalid input message.

---
+++ components/user/record.js
@@ -0,0 +1,25 @@
+export const DefaultRecord = {
+    name: '',
+    email: '',
+    password: '',
+    active_company_id: '',
+    active_branch_id: '',
+    phone: '',
+    address: '',
+    city: '',
+    country: '',
+    postal_code: ''
+}
+
+export const DefaultInvalidInputMessage = {
+    name: [],
+    email: [],
+    password: [],
+    active_company_id: [],
+    active_branch_id: [],
+    phone: [],
+    address: [],
+    city: [],
+    country: [],
+    postal_code: []
+}
