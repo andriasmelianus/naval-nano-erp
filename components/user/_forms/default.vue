f4c12366c121c00cf92f006e4bba4e9aecd30afb
Andrias Melianus Siwabessy <andriasmelianus@gmail.com>
2021-05-15 09:39:34 +0700

    Add user default form.

---
+++ components/user/_forms/default.vue
@@ -0,0 +1,63 @@
+<template>
+  <v-form @submit.prevent="submitForm" class="pa-4">
+    <div class="text-h6">Data Pengguna</div>
+
+    <v-text-field
+      label="Nama Pengguna"
+      v-model="record.name"
+      :error-messages="invalidInputMessage.name"
+    ></v-text-field>
+
+    <v-text-field
+      label="Email"
+      v-model="record.email"
+      :error-messages="invalidInputMessage.email"
+    ></v-text-field>
+
+    <v-text-field
+      label="Password"
+      type="password"
+      v-model="record.password"
+      :error-messages="invalidInputMessage.password"
+    ></v-text-field>
+
+    <v-text-field
+      label="Telepon"
+      v-model="record.phone"
+      :error-messages="invalidInputMessage.phone"
+    ></v-text-field>
+
+    <v-text-field
+      label="Alamat"
+      v-model="record.address"
+      :error-messages="invalidInputMessage.address"
+    ></v-text-field>
+
+    <v-text-field
+      label="Kota"
+      v-model="record.city"
+      :error-messages="invalidInputMessage.city"
+    ></v-text-field>
+
+    <v-text-field
+      label="Negara"
+      v-model="record.country"
+      :error-messages="invalidInputMessage.country"
+    ></v-text-field>
+
+    <v-text-field
+      label="Kode Pos"
+      v-model="record.postal_code"
+      :error-messages="invalidInputMessage.postal_code"
+    ></v-text-field>
+
+    <v-btn type="submit" color="success">Simpan</v-btn>
+  </v-form>
+</template>
+
+<script>
+import { Handler } from "./handler";
+export default {
+  mixins: [Handler],
+};
+</script>
