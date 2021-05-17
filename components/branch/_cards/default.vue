554b72399fc092a67473ce5984be5e68de694685
Andrias Melianus Siwabessy <andriasmelianus@gmail.com>
2021-05-17 08:32:20 +0700

    Add named slot for content and actions section.

--- components/branch/_cards/default.vue
+++ components/branch/_cards/default.vue
@@ -10,45 +10,49 @@
       </v-btn>
     </v-app-bar>
 
-    <v-list-item>
-      <v-list-item-subtitle>Nama Cabang</v-list-item-subtitle>
-      <v-list-item-title>{{ record.name }}</v-list-item-title>
-    </v-list-item>
+    <slot>
+      <v-list-item>
+        <v-list-item-subtitle>Nama Cabang</v-list-item-subtitle>
+        <v-list-item-title>{{ record.name }}</v-list-item-title>
+      </v-list-item>
 
-    <v-list-item dense>
-      <v-list-item-subtitle>Kode Cabang</v-list-item-subtitle>
-      <v-list-item-title>{{ record.code }}</v-list-item-title>
-    </v-list-item>
+      <v-list-item dense>
+        <v-list-item-subtitle>Kode Cabang</v-list-item-subtitle>
+        <v-list-item-title>{{ record.code }}</v-list-item-title>
+      </v-list-item>
 
-    <v-list-item dense>
-      <v-list-item-subtitle>Telepon</v-list-item-subtitle>
-      <v-list-item-title>{{ record.phone }}</v-list-item-title>
-    </v-list-item>
+      <v-list-item dense>
+        <v-list-item-subtitle>Telepon</v-list-item-subtitle>
+        <v-list-item-title>{{ record.phone }}</v-list-item-title>
+      </v-list-item>
 
-    <v-list-item dense>
-      <v-list-item-subtitle>Alamat</v-list-item-subtitle>
-      <v-list-item-title>{{ record.address }}</v-list-item-title>
-    </v-list-item>
+      <v-list-item dense>
+        <v-list-item-subtitle>Alamat</v-list-item-subtitle>
+        <v-list-item-title>{{ record.address }}</v-list-item-title>
+      </v-list-item>
 
-    <v-list-item dense>
-      <v-list-item-subtitle>Kota</v-list-item-subtitle>
-      <v-list-item-title>{{ record.city }}</v-list-item-title>
-    </v-list-item>
+      <v-list-item dense>
+        <v-list-item-subtitle>Kota</v-list-item-subtitle>
+        <v-list-item-title>{{ record.city }}</v-list-item-title>
+      </v-list-item>
 
-    <v-list-item dense>
-      <v-list-item-subtitle>Negara</v-list-item-subtitle>
-      <v-list-item-title>{{ record.country }}</v-list-item-title>
-    </v-list-item>
+      <v-list-item dense>
+        <v-list-item-subtitle>Negara</v-list-item-subtitle>
+        <v-list-item-title>{{ record.country }}</v-list-item-title>
+      </v-list-item>
 
-    <v-list-item dense>
-      <v-list-item-subtitle>Kode Pos</v-list-item-subtitle>
-      <v-list-item-title>{{ record.postal_code }}</v-list-item-title>
-    </v-list-item>
+      <v-list-item dense>
+        <v-list-item-subtitle>Kode Pos</v-list-item-subtitle>
+        <v-list-item-title>{{ record.postal_code }}</v-list-item-title>
+      </v-list-item>
+    </slot>
 
     <v-divider></v-divider>
 
     <v-card-actions>
-      <v-btn text nuxt :to="'/branch/' + record.id">Edit</v-btn>
+      <slot name="actions">
+        <v-btn text nuxt :to="'/branch/' + record.id">Edit</v-btn>
+      </slot>
     </v-card-actions>
   </v-card>
 </template>
--- components/company/_cards/default.vue
+++ components/company/_cards/default.vue
@@ -10,53 +10,57 @@
       </v-btn>
     </v-app-bar>
 
-    <v-list-item>
-      <v-list-item-subtitle>Nama Perusahaan</v-list-item-subtitle>
-      <v-list-item-title>{{ record.name }}</v-list-item-title>
-    </v-list-item>
+    <slot>
+      <v-list-item>
+        <v-list-item-subtitle>Nama Perusahaan</v-list-item-subtitle>
+        <v-list-item-title>{{ record.name }}</v-list-item-title>
+      </v-list-item>
 
-    <v-list-item dense>
-      <v-list-item-subtitle>Nama Terdaftar</v-list-item-subtitle>
-      <v-list-item-title>{{ record.legal_name }}</v-list-item-title>
-    </v-list-item>
+      <v-list-item dense>
+        <v-list-item-subtitle>Nama Terdaftar</v-list-item-subtitle>
+        <v-list-item-title>{{ record.legal_name }}</v-list-item-title>
+      </v-list-item>
 
-    <v-list-item dense>
-      <v-list-item-subtitle>Email</v-list-item-subtitle>
-      <v-list-item-title>
-        <a :href="'mailto:' + record.email">{{
-          record.email
-        }}</a></v-list-item-title
-      >
-    </v-list-item>
+      <v-list-item dense>
+        <v-list-item-subtitle>Email</v-list-item-subtitle>
+        <v-list-item-title>
+          <a :href="'mailto:' + record.email">{{
+            record.email
+          }}</a></v-list-item-title
+        >
+      </v-list-item>
 
-    <v-list-item dense>
-      <v-list-item-subtitle>Website</v-list-item-subtitle>
-      <v-list-item-title>
-        <a :href="record.website" target="_blank" rel="noopener noreferrer">{{
-          record.website
-        }}</a>
-      </v-list-item-title>
-    </v-list-item>
+      <v-list-item dense>
+        <v-list-item-subtitle>Website</v-list-item-subtitle>
+        <v-list-item-title>
+          <a :href="record.website" target="_blank" rel="noopener noreferrer">{{
+            record.website
+          }}</a>
+        </v-list-item-title>
+      </v-list-item>
 
-    <v-list-item dense>
-      <v-list-item-subtitle>Telepon</v-list-item-subtitle>
-      <v-list-item-title>{{ record.phone }}</v-list-item-title>
-    </v-list-item>
+      <v-list-item dense>
+        <v-list-item-subtitle>Telepon</v-list-item-subtitle>
+        <v-list-item-title>{{ record.phone }}</v-list-item-title>
+      </v-list-item>
 
-    <v-list-item dense>
-      <v-list-item-subtitle>Deskripsi Singkat</v-list-item-subtitle>
-      <v-list-item-title>{{ record.short_description }}</v-list-item-title>
-    </v-list-item>
+      <v-list-item dense>
+        <v-list-item-subtitle>Deskripsi Singkat</v-list-item-subtitle>
+        <v-list-item-title>{{ record.short_description }}</v-list-item-title>
+      </v-list-item>
 
-    <v-list-item dense>
-      <v-list-item-subtitle>Deskripsi</v-list-item-subtitle>
-      <v-list-item-title v-text="record.description"></v-list-item-title>
-    </v-list-item>
+      <v-list-item dense>
+        <v-list-item-subtitle>Deskripsi</v-list-item-subtitle>
+        <v-list-item-title v-text="record.description"></v-list-item-title>
+      </v-list-item>
+    </slot>
 
     <v-divider></v-divider>
 
     <v-card-actions>
-      <v-btn text nuxt :to="'/company'">Edit</v-btn>
+      <slot name="actions">
+        <v-btn text nuxt :to="'/company'">Ubah</v-btn>
+      </slot>
     </v-card-actions>
   </v-card>
 </template>
