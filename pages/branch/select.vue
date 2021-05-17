adb6c1676e79ca607523b923bef830f3f91873c2
Andrias Melianus Siwabessy <andriasmelianus@gmail.com>
2021-05-17 10:05:37 +0700

    Add select pages to change company and branch.

---
+++ pages/branch/select.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-row class="fill-height">
+    <v-col cols="12" md="12">
+      <div class="text-h5 pb-4">Ganti Cabang Aktif</div>
+      <branch-select-data-iterator></branch-select-data-iterator>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import BranchSelectDataIterator from "~/components/branch/_data-iterators/select";
+export default {
+  layout: "dashboard",
+
+  head() {
+    return {
+      title: "Ganti Cabang Aktif",
+    };
+  },
+
+  components: {
+    BranchSelectDataIterator,
+  },
+};
+</script>
---
+++ pages/company/select.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-row class="fill-height">
+    <v-col cols="12" md="12">
+      <div class="text-h5 pb-4">Ganti Perusahaan Aktif</div>
+      <company-select-data-iterator></company-select-data-iterator>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import CompanySelectDataIterator from "~/components/company/_data-iterators/select";
+export default {
+  layout: "dashboard",
+
+  head() {
+    return {
+      title: "Ganti Perusahaan Aktif",
+    };
+  },
+
+  components: {
+    CompanySelectDataIterator,
+  },
+};
+</script>
