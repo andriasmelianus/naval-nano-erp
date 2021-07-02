85724ab5c37b3e59412ac3b5c2dd9d8809db8980
Andrias Melianus Siwabessy <andriasmelianus@gmail.com>
2021-07-02 22:15:00 +0700

    Add company charts component with dummy data.

---
+++ components/company/charts/monthly-performance-1.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="amcharts4div" :style="divStyle"></div>
+</template>
+
+<script>
+import { amcharts4 } from "~/components/_mixins/amcharts4";
+export default {
+  mixins: [amcharts4],
+
+  mounted() {
+    let vm = this;
+
+    // Chart configuration
+    vm.chart = vm.am4core.create("amcharts4div", vm.am4charts.PieChart);
+    vm.chart.innerRadius = vm.am4core.percent(50);
+    // Chart series configuration
+    vm.chartSeries = vm.chart.series.push(new vm.am4charts.PieSeries());
+    vm.chartSeries.dataFields.value = "value";
+    vm.chartSeries.dataFields.category = "category";
+    vm.chartSeries.labels.template.text = "{category}: {value}";
+    // Chart legend configuration
+    vm.chart.legend = new vm.am4charts.Legend();
+    vm.chart.legend.position = "left";
+
+    // Chart data configuration
+    vm.data = [
+      {
+        category: "Consumer Goods",
+        value: 89,
+      },
+      {
+        category: "Basic Components",
+        value: 29,
+      },
+      {
+        category: "Spare Parts",
+        value: 64,
+      },
+    ];
+    vm.chart.data = vm.data;
+  },
+};
+</script>
