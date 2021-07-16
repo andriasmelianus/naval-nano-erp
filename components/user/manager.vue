cedfccf3eb08fc43b6370708446ef96ffdede744
Andrias Melianus Siwabessy <andriasmelianus@gmail.com>
2021-07-16 17:51:21 +0700

    Add user manager component.

---
+++ components/user/manager.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-card>
+    <v-row>
+      <v-col cols="12" md="7">
+        <user-default-table
+          @record-selected="handleRecordSelected"
+        ></user-default-table>
+      </v-col>
+      <v-col cols="12" md="5">
+        <role-user-table :user="user"></role-user-table>
+      </v-col>
+    </v-row>
+  </v-card>
+</template>
+
+<script>
+import UserDefaultTable from "./tables/default.vue";
+import RoleUserTable from "~/components/role/tables/role-user.vue";
+export default {
+  components: {
+    UserDefaultTable,
+    RoleUserTable,
+  },
+
+  data: () => ({
+    user: {},
+  }),
+
+  methods: {
+    /**
+     * Send the selected user to role-user component.
+     * @returns void
+     */
+    handleRecordSelected(e) {
+      if (e.value) {
+        this.user = e.item;
+      } else {
+        this.user = {};
+      }
+    },
+  },
+};
+</script>
