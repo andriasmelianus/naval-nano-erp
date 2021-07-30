a7b864351a34d4f00084984a2fbec8ad3552b591
Andrias Melianus Siwabessy <andriasmelianus@gmail.com>
2021-07-30 21:44:25 +0700

    Add user autocomplete component.

---
+++ components/user/autocomplete/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-autocomplete
+    label="Pilih salah satu..."
+    item-text="email"
+    item-value="email"
+    :search-input.sync="searchKeyword"
+    :loading="isLoading"
+    :error-messages="invalidInputMessage.email"
+    :items="records"
+    @change="handleChange"
+    no-filter
+    return-object
+    hide-no-data
+    auto-select-first
+  ></v-autocomplete>
+</template>
+
+<script>
+import { Handler } from "./handler";
+export default {
+  mixins: [Handler],
+};
+</script>
