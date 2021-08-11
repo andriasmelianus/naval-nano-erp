ad74d34846cf7a0fa110516608d33a0f49c3b3b9
Andrias Melianus Siwabessy <andriasmelianus@gmail.com>
2021-08-11 15:43:24 +0700

    Add image card component to display single image.

---
+++ components/image/cards/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-card>
+    <v-img
+      :src="source"
+      :width="width ? width : '100%'"
+      :height="height"
+    ></v-img>
+  </v-card>
+</template>
+
+<script>
+import { Handler } from "./handler";
+export default {
+  mixins: [Handler],
+};
+</script>
