a8c6d40f1550c23385b365baf7dec297e868a40d
Andrias Melianus Siwabessy <andriasmelianus@gmail.com>
2021-08-19 22:22:49 +0700

    Add no role message.

--- components/role/lists/role-user.vue
+++ components/role/lists/role-user.vue
@@ -8,6 +8,12 @@
         <v-list-item-subtitle>{{ record.description }}</v-list-item-subtitle>
       </v-list-item-content>
     </v-list-item>
+
+    <v-list-item v-if="records.length == 0">
+      <v-list-item-content>
+        <v-list-item-title>Tidak memiliki role.</v-list-item-title>
+      </v-list-item-content>
+    </v-list-item>
   </v-list>
 </template>
 
