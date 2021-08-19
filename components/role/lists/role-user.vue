2a18380cb9d3ece6ace39e1f09d9cb0f16a20549
Andrias Melianus Siwabessy <andriasmelianus@gmail.com>
2021-08-19 21:45:05 +0700

    Add role user component using Vuetify list.

---
+++ components/role/lists/role-user.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-list two-line>
+    <v-subheader>Role Saya</v-subheader>
+
+    <v-list-item v-for="record in records" :key="record.id">
+      <v-list-item-content>
+        <v-list-item-title>{{ record.name }}</v-list-item-title>
+        <v-list-item-subtitle>{{ record.description }}</v-list-item-subtitle>
+      </v-list-item-content>
+    </v-list-item>
+  </v-list>
+</template>
+
+<script>
+import { Handler } from "./handler";
+export default {
+  mixins: [Handler],
+};
+</script>
