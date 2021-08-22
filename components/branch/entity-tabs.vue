9b430ca71cf8ed7fdb14cdc1925f50ab48ec8d51
Andrias Melianus Siwabessy <andriasmelianus@gmail.com>
2021-08-22 21:00:08 +0700

    Add asset small table to the entity tabs component.

--- components/branch/entity-tabs.vue
+++ components/branch/entity-tabs.vue
@@ -2,16 +2,20 @@
   <v-card>
     <v-tabs vertical>
       <v-tab>
-        <v-icon left>mdi-account</v-icon>
+        <v-icon left>mdi-account-lock</v-icon>
         Pengguna
       </v-tab>
       <v-tab>
-        <v-icon left>mdi-page-next</v-icon>
+        <v-icon left>mdi-account-supervisor-circle</v-icon>
+        Karyawan
+      </v-tab>
+      <v-tab>
+        <v-icon left>mdi-door-open</v-icon>
         Ruangan
       </v-tab>
       <v-tab>
-        <v-icon left>mdi-broom</v-icon>
-        Karyawan
+        <v-icon left>mdi-chair-rolling</v-icon>
+        Aset
       </v-tab>
 
       <v-tab-item>
@@ -21,12 +25,17 @@
       </v-tab-item>
       <v-tab-item>
         <v-card flat>
+          <employee-default-small-table></employee-default-small-table>
+        </v-card>
+      </v-tab-item>
+      <v-tab-item>
+        <v-card flat>
           <room-default-small-table></room-default-small-table>
         </v-card>
       </v-tab-item>
       <v-tab-item>
         <v-card flat>
-          <employee-default-small-table></employee-default-small-table>
+          <asset-default-small-table></asset-default-small-table>
         </v-card>
       </v-tab-item>
     </v-tabs>
@@ -34,14 +43,16 @@
 </template>
 
 <script>
-import UserDefaultSmallTable from "~/components/user/tables/default-small";
-import RoomDefaultSmallTable from "~/components/room/tables/default-small";
-import EmployeeDefaultSmallTable from "~/components/employee/tables/default-small";
+import UserDefaultSmallTable from "~/components/user/tables/default-small.vue";
+import EmployeeDefaultSmallTable from "~/components/employee/tables/default-small.vue";
+import RoomDefaultSmallTable from "~/components/room/tables/default-small.vue";
+import AssetDefaultSmallTable from "~/components/asset/tables/default-small.vue";
 export default {
   components: {
     UserDefaultSmallTable,
     RoomDefaultSmallTable,
     EmployeeDefaultSmallTable,
+    AssetDefaultSmallTable,
   },
 };
 </script>
