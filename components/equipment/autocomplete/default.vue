a020256ab5343dac73442ab6fab1db646aceab00
Andrias Melianus Siwabessy <andriasmelianus@gmail.com>
2021-08-24 22:04:35 +0700

    Add equipment autocomplete component.

---
+++ components/equipment/autocomplete/default.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-autocomplete
+    label="Peralatan"
+    item-text="name"
+    item-value="id"
+    hint="Ketik 3 karakter untuk mulai mencari"
+    append-icon=""
+    :search-input.sync="searchKeyword"
+    :loading="isLoading"
+    :error-messages="invalidInputMessage.email"
+    :items="records"
+    @change="handleChange"
+    clearable
+    hide-selected
+    no-filter
+    return-object
+    hide-no-data
+    auto-select-first
+  >
+    <template v-slot:item="{ parent, item }">
+      <div>
+        {{ item.name }} - ({{ item.code }})
+        <br />
+        <span
+          class="text-caption"
+          v-text="
+            item.description.length > 50
+              ? item.description.substring(0, 50) + '...'
+              : item.description
+          "
+        ></span>
+      </div>
+    </template>
+  </v-autocomplete>
+</template>
+
+<script>
+import { Handler } from "./handler";
+export default {
+  mixins: [Handler],
+};
+</script>
---
+++ components/equipment/autocomplete/handler.js
@@ -0,0 +1,23 @@
+import { GlobalAutocompleteHandler } from "~/components/_mixins/global-autocomplete-handler";
+import {
+  DefaultRecord,
+  DefaultInvalidInputMessage
+} from "~/components/user/record";
+export const Handler = {
+  mixins: [GlobalAutocompleteHandler],
+
+  props: {
+    record: {
+      type: Object,
+      default: function() {
+        return DefaultRecord;
+      }
+    }
+  },
+
+  data: () => ({
+    defaultInvalidInputMessage: DefaultInvalidInputMessage,
+
+    resourceUri: "/equipment"
+  })
+};
