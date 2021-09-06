978cfe483aee36e64f497e242d54f5dbebc45b63
Andrias Melianus Siwabessy <andriasmelianus@gmail.com>
2021-09-06 21:20:57 +0700

    Add combobox control for brand.

---
+++ components/brand/combobox/default.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-combobox
+    item-text="name"
+    item-value="name"
+    append-icon=""
+    :hint="hintText"
+    :search-input.sync="searchKeyword"
+    :loading="isLoading"
+    :items="records"
+    clearable
+    hide-selected
+    no-filter
+    hide-no-data
+    v-bind="$attrs"
+    v-on="$listeners"
+  ></v-combobox>
+</template>
+
+<script>
+import { Handler } from "./handler";
+export default {
+  mixins: [Handler],
+};
+</script>
