4d1530917fc5dfb2329c5d3f343522b3b6146f75
Andrias Melianus Siwabessy <andriasmelianus@gmail.com>
2021-09-10 06:33:12 +0700

    Add package: probil/v-mask.

--- nuxt.config.js
+++ nuxt.config.js
@@ -44,7 +44,7 @@ export default {
   css: ["@/assets/main.css", "@mdi/font/css/materialdesignicons.min.css"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ["~/plugins/axios"],
+  plugins: ["~/plugins/axios", "~/plugins/v-mask"],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
