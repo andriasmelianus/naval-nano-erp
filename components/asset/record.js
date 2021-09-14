36b1808f6d57602b604736edde1fd1839fdcf639
Andrias Melianus Siwabessy <andriasmelianus@gmail.com>
2021-09-15 00:08:06 +0700

    Change DefaultRecord default value from empty string to undefined.

--- components/asset/record.js
+++ components/asset/record.js
@@ -1,13 +1,13 @@
 export const DefaultRecord = {
-  equipment_id: "",
-  brand: "",
-  model: "",
-  serial_number: "",
-  code: "",
-  purchase_date: "",
-  price: "",
-  is_deprecated: "",
-  note: ""
+  equipment_id: undefined,
+  brand: undefined,
+  model: undefined,
+  serial_number: undefined,
+  code: undefined,
+  purchase_date: undefined,
+  price: undefined,
+  is_deprecated: undefined,
+  note: undefined
 };
 
 export const DefaultInvalidInputMessage = {
