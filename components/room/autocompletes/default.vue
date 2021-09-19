447780af6caab2a543b394eb74b7096fccc248e4
Andrias Melianus Siwabessy <andriasmelianus@gmail.com>
2021-09-19 20:31:22 +0700

    Add autocomplete component for room module.

---
+++ components/room/autocompletes/default.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-autocomplete
+    v-model="value"
+    item-text="name"
+    item-value="id"
+    append-icon=""
+    :hint="hintText"
+    :search-input.sync="searchKeyword"
+    :loading="isLoading"
+    :items="records"
+    clearable
+    hide-selected
+    no-filter
+    hide-no-data
+    auto-select-first
+    v-bind="$attrs"
+    v-on="$listeners"
+  ></v-autocomplete>
+</template>
+
+<script>
+import { Handler } from "./handler";
+export default {
+  inheritAttrs: false,
+
+  mixins: [Handler],
+};
+</script>
