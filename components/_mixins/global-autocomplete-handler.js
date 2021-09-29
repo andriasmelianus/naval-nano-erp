abca4a3e416aa5758dc639c2d59941d78e64efda
andriasmelianus <andriasmelianus@gmail.com>
2021-09-29 17:12:41 +0700

    Generalize mixin for autocomplete and combobox components.

--- components/_mixins/global-autocomplete-handler.js
+++ components/_mixins/global-autocomplete-handler.js
@@ -1,5 +1,9 @@
 import { MessageExtractor } from "./message-extractor";
 import { InvalidInputMessageHandler } from "./form/invalid-input-message-handler";
+
+/**
+ * This mixin can be used for autocomplete and combobox components.
+ */
 export const GlobalAutocompleteHandler = {
   mixins: [MessageExtractor, InvalidInputMessageHandler],
 
@@ -17,17 +21,25 @@ export const GlobalAutocompleteHandler = {
     },
 
     /**
-     * Parameter name for value to send within request.
+     * Value field of the selectedRecord object.
      */
-    valueParameterName: {
+    itemValue: {
       type: String,
-      default: "value"
+      default: "id"
     },
 
     /**
-     * Parameter name to send within request.
+     * Text field of the selectedRecord object.
      */
-    searchKeywordParameterName: {
+    itemText: {
+      type: String,
+      default: "name"
+    },
+
+    /**
+     * Request parameter name for search keyword.
+     */
+    searchKeywordRequestParameterName: {
       type: String,
       default: "searchKeyword"
     },
@@ -53,15 +65,16 @@ export const GlobalAutocompleteHandler = {
     }
   },
 
-  data: () => ({
-    searchKeyword: "",
+  data: vm => ({
+    records: [],
+    selectedRecord: undefined, // Set to component v-model.
+    isSingleRecordSearch: false,
+
+    searchKeyword: undefined,
     serverParams: {},
-
-    records: undefined,
-
     isLoading: false,
-    dataFetchTimerId: null,
-    dataFetchTimerDelay: 750
+    dataFetchTimerId: undefined,
+    dataFetchTimerDelay: 600
   }),
 
   computed: {
@@ -71,44 +84,46 @@ export const GlobalAutocompleteHandler = {
       );
     },
 
+    hideNoData() {
+      return !this.searchKeyword;
+    },
+
     resourceUriForAutocomplete() {
       return this.resourceUri + this.componentUri;
     }
   },
 
   watch: {
-    /**
-     * Update server params in order to perform request
-     * to retrieve selected record.
-     */
     value(newValue, oldValue) {
-      let vm = this;
-
-      if (newValue != null) {
-        if (typeof newValue == "object") {
-          vm.updateServerParams({
-            [vm.valueParameterName]: newValue[vm.valueParameterName]
-          });
-        } else {
-          vm.updateServerParams({
-            [vm.valueParameterName]: newValue
-          });
-        }
-      }
+      this.handleValueChanged(newValue);
     },
 
     /**
-     * Begin data search when keyword is typed.
+     * Perform API request when search keyword is typed.
      */
     searchKeyword(newValue, oldValue) {
       let vm = this;
 
+      /**
+       * Prevent performing unnecessary request if the this.selectedRecord[this.itemText]
+       * has the same value with this.searchKeyword.
+       */
+      if (vm.selectedRecord != undefined) {
+        // Prevent error while trying to access itemText from null.
+        if (newValue == vm.selectedRecord[vm.itemText]) {
+          return;
+        }
+      }
+
       if (newValue != null) {
         if (newValue.length >= vm.minimumCharacters) {
+          vm.isSingleRecordSearch = false;
           vm.updateServerParams({
-            [vm.searchKeywordParameterName]: newValue
+            [vm.searchKeywordRequestParameterName]: newValue
           });
         }
+      } else {
+        vm.selectedRecord = undefined;
       }
     },
 
@@ -123,10 +138,50 @@ export const GlobalAutocompleteHandler = {
         this.readRecords();
       },
       deep: true
+    },
+
+    /**
+     * Emit input event with the value.
+     * @param {mixed} newValue New selected record
+     * @param {mixed} oldValue Old selected record
+     */
+    selectedRecord(newValue, oldValue) {
+      let vm = this;
+
+      if (typeof newValue == "object") {
+        vm.$emit("input", newValue[vm.itemValue]);
+      } else {
+        vm.$emit("input", newValue);
+      }
     }
   },
 
+  mounted() {
+    this.handleValueChanged(this.value);
+  },
+
   methods: {
+    /**
+     * Handle operation if value prop is updated/changed.
+     * @param {mixed} newValue Value provided from v-model.
+     */
+    handleValueChanged(newValue) {
+      let vm = this;
+
+      if (newValue == undefined) {
+        vm.selectedRecord = undefined;
+      } else {
+        if (typeof newValue == "object") {
+          vm.selectedRecord = newValue;
+        } else {
+          vm.isSingleRecordSearch = true;
+          vm.updateServerParams({
+            [vm.itemValue]: newValue
+          });
+        }
+      }
+    },
+
     /**
      * Set the new value for server parameters.
      * @param {Object} params
@@ -137,7 +192,7 @@ export const GlobalAutocompleteHandler = {
 
       clearTimeout(vm.dataFetchTimerId);
       vm.dataFetchTimerId = setTimeout(function() {
-        vm.serverParams = Object.assign({}, vm.serverParams, params);
+        vm.serverParams = Object.assign({}, params);
       }, vm.dataFetchTimerDelay);
     },
 
@@ -156,6 +211,18 @@ export const GlobalAutocompleteHandler = {
         .then(function(result) {
           vm.records = result;
           vm.isLoading = false;
+
+          /**
+           * If user provide value for v-model, this component will search a single record that match the value in v-model.
+           * Request will be sent with parameter defined in the props: itemValue.
+           * If system found the record, it will assign the record as selectedRecord.
+           */
+          if (vm.records.length) {
+            if (vm.isSingleRecordSearch) {
+              // Identify whether the call is from v-model or searchKeyword change event.
+              vm.selectedRecord = vm.records[0];
+            }
+          }
         })
         .catch(function(result) {
           vm.isLoading = false;
@@ -174,14 +241,6 @@ export const GlobalAutocompleteHandler = {
         vm.invalidInputMessage,
         vm.defaultInvalidInputMessage
       );
-    },
-
-    /**
-     * Capture the selected item change from v-autocomplete control.
-     * @return {void}
-     */
-    handleChange(selectedRecord) {
-      this.$emit("selected-change", selectedRecord);
     }
   }
 };
