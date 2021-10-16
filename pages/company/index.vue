d7b1c4ccac2344926755775d28e985d14bae4e57
Andrias Melianus Siwabessy <andriasmelianus@gmail.com>
2021-10-17 00:21:40 +0700

    Add company-related component on company page.

--- pages/company/index.vue
+++ pages/company/index.vue
@@ -6,6 +6,55 @@
     </template>
 
     <v-card>
+      <v-tabs v-model="selectedTab" vertical>
+        <v-tab>Perusahaan</v-tab>
+        <v-tab>Cabang</v-tab>
+        <v-tab>Ruangan</v-tab>
+        <v-tab>Karyawan</v-tab>
+
+        <v-tab-item>
+          <v-card flat>
+            <company-form
+              :record="companyData"
+              edit-mode
+              @record-updated="companyUpdated"
+            ></company-form>
+          </v-card>
+        </v-tab-item>
+
+        <v-tab-item>
+          <v-card flat>
+            <branch-table></branch-table>
+          </v-card>
+        </v-tab-item>
+
+        <v-tab-item>
+          <v-card flat>
+            <room-table></room-table>
+
+            <p class="pa-3 text-caption">
+              Data ruangan yang ditampilkan merupakan data ruangan pada cabang
+              yang sedang aktif. <br />
+              Untuk melihat data ruangan di cabang lain, Anda harus mengganti
+              cabang yang aktif ke cabang yang dimaksud.
+            </p>
+          </v-card>
+        </v-tab-item>
+
+        <v-tab-item>
+          <v-card flat>
+            <employee-table></employee-table>
+
+            <p class="pa-3 text-caption">
+              Data karyawan yang ditampilkan pada halaman ini merupakan data
+              karyawan pada cabang yang sedang aktif.
+            </p>
+          </v-card>
+        </v-tab-item>
+      </v-tabs>
+    </v-card>
+
+    <!-- <v-card>
       <v-row>
         <v-col cols="12" md="6">
           <company-form
@@ -18,15 +67,17 @@
           <branch-small-table></branch-small-table>
         </v-col>
       </v-row>
-    </v-card>
+    </v-card> -->
   </page-content>
 </template>
 
 <script>
 import PageContent from "~/components/_support/page-content.vue";
 import { MessageExtractor } from "~/components/_mixins/message-extractor";
-import CompanyForm from "~/components/company/forms/default";
-import BranchSmallTable from "~/components/branch/tables/default-small";
+import CompanyForm from "~/components/company/forms/default.vue";
+import BranchTable from "~/components/branch/tables/default.vue";
+import RoomTable from "~/components/room/tables/default.vue";
+import EmployeeTable from "~/components/employee/tables/default.vue";
 export default {
   layout: "dashboard",
 
@@ -41,10 +92,13 @@ export default {
   components: {
     PageContent,
     CompanyForm,
-    BranchSmallTable,
+    BranchTable,
+    RoomTable,
+    EmployeeTable,
   },
 
   data: () => ({
+    selectedTab: undefined,
     companyData: {},
   }),
 
