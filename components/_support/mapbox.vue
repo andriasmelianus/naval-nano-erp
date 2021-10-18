2f9af472826c7e2cfceea40aac91600faddc693f
Andrias Melianus Siwabessy <andriasmelianus@gmail.com>
2021-10-18 23:45:15 +0700

    Complete Mapbox component for selecting point.

--- components/_support/mapbox.vue
+++ components/_support/mapbox.vue
@@ -1,12 +1,14 @@
 <template>
-  <dir id="map" :style="divStyle"></dir>
+  <dir :id="mapDivId" :style="divStyle"></dir>
 </template>
 
 <script>
+import { Mapbox } from "~/components/_mixins/mapbox";
 import mapboxgl from "mapbox-gl";
+import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
 import { MessageExtractor } from "~/components/_mixins/message-extractor";
 export default {
-  mixins: [MessageExtractor],
+  mixins: [Mapbox, MessageExtractor],
 
   props: {
     width: {
@@ -18,77 +20,70 @@ export default {
       type: String,
       default: "100%",
     },
-
-    // Initial latitude.
-    latitude: {
-      type: [Number, String],
-      required: true,
-    },
-
-    // Initial longitude.
-    longitude: {
-      type: [Number, String],
-      required: true,
-    },
-
-    // Initial zoom of the map.
-    zoom: {
-      type: [Number, String],
-      default: 8,
-    },
-
-    resourceUriToGetToken: {
-      type: String,
-      default: "/support/mapbox-token",
-    },
   },
 
   data: (vm) => ({
-    map: undefined,
+    marker: undefined,
   }),
 
   computed: {
     divStyle() {
       return "width: " + this.width + ";height: " + this.height;
     },
-
-    /**
-     * Initial latitude and longitude as array.
-     */
-    initialLtLn() {
-      return [this.latitude, this.longitude];
-    },
-
-    /**
-     * Initial longitude and latitude as array.
-     */
-    initialLnLt() {
-      return [this.longitude, this.latitude];
-    },
   },
 
   mounted() {
     let vm = this;
-    vm.$axios
-      .$get(vm.resourceUriToGetToken)
-      .then(function (result) {
-        mapboxgl.accessToken = result;
-        vm.map = new mapboxgl.Map({
-          container: "map",
-          style: "mapbox://styles/mapbox/streets-v11",
-          center: vm.initialLnLt,
-          zoom: vm.zoom,
-        });
+
+    // Initialize geocoder.
+    vm.map.addControl(
+      new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl: vm.map,
       })
-      .catch(function (result) {
-        vm.$store.commit("global-snackbar/show", {
-          color: "error",
-          message: vm.messageErrorExtract(result),
-        });
-      });
+    );
+
+    // Initialize marker.
+    vm.marker = new mapboxgl.Marker({
+      draggable: true,
+    })
+      .setLngLat(vm.initialLnLt)
+      .on("dragend", vm.handleMarkerDragged)
+      .addTo(vm.map);
+
+    // Add click event to map.
+    vm.map.on("click", vm.handleMapClicked);
+  },
+
+  methods: {
+    /**
+     * Catch the click event on the map.
+     * Emits point-selected event when map is clicked.
+     * @param {Object} e
+     * @return {void}
+     */
+    handleMapClicked(e) {
+      this.marker.setLngLat(e.lngLat);
+      this.$emit("point-selected", [e.lngLat.lat, e.lngLat.lng]);
+    },
+
+    /**
+     * Catch drag event on marker.
+     * Emits point-selected event when marker is dragged.
+     * @param {Object} e
+     * @return {void}
+     */
+    handleMarkerDragged(e) {
+      this.$emit("point-selected", [
+        e.target._lngLat.lat,
+        e.target._lngLat.lng,
+      ]);
+    },
   },
 };
 </script>
 
 <style src="mapbox-gl/dist/mapbox-gl.css">
+</style>
+<style src="@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css">
 </style>
