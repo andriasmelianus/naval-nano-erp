5add55dee4602f773cbe4a8e1be57f746c608ac5
Andrias Melianus Siwabessy <andriasmelianus@gmail.com>
2021-11-01 20:29:28 +0700

    Add submission index page.

---
+++ pages/submission/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <page-content>
+    <template v-slot:title>{{ $metaInfo.title }}</template>
+    <template v-slot:subtitle
+      >Halaman ini memungkinkan untuk melakukan pengajuan permintaan baik
+      pengadaan aset, perbaikan aset dan sebagainya.</template
+    >
+  </page-content>
+</template>
+
+<script>
+import PageContent from "~/components/_support/page-content.vue";
+export default {
+  layout: "dashboard",
+
+  head() {
+    return {
+      title: "Pengajuan Permintaan",
+    };
+  },
+
+  components: {
+    PageContent,
+  },
+};
+</script>
