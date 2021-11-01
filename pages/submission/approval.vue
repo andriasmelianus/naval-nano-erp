6c0a009c41506814d24d0e501ff9230da90ed85f
Andrias Melianus Siwabessy <andriasmelianus@gmail.com>
2021-11-01 20:29:41 +0700

    Add submission approval page.

---
+++ pages/submission/approval.vue
@@ -0,0 +1,26 @@
+<template>
+  <page-content>
+    <template v-slot:title>{{ $metaInfo.title }}</template>
+    <template v-slot:subtitle
+      >Pengajuan-pengajuan yang telah diinput dapat disetujui melalui halaman
+      ini.</template
+    >
+  </page-content>
+</template>
+
+<script>
+import PageContent from "~/components/_support/page-content.vue";
+export default {
+  layout: "dashboard",
+
+  head() {
+    return {
+      title: "Persetujuan Pengajuan",
+    };
+  },
+
+  components: {
+    PageContent,
+  },
+};
+</script>
