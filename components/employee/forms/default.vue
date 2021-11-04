2639720d7cc877f968f54a098d57006a6f08f75f
Andrias Melianus Siwabessy <andriasmelianus@gmail.com>
2021-11-04 21:57:49 +0700

    Reorder the properties position for readability.

--- components/employee/forms/default.vue
+++ components/employee/forms/default.vue
@@ -11,66 +11,66 @@
     <v-row>
       <v-col cols="12" md="4">
         <v-select
-          label="Jenis Identitas"
-          :items="id_types"
-          item-value="text"
           v-model="record.id_type"
+          label="Jenis Identitas"
+          item-value="text"
+          :items="id_types"
           :error-messages="invalidInputMessage.id_type"
         ></v-select>
       </v-col>
 
       <v-col cols="12" md="8">
         <v-text-field
-          label="Nomor Identitas"
           v-model="record.id_number"
+          label="Nomor Identitas"
           :error-messages="invalidInputMessage.id_number"
         ></v-text-field>
       </v-col>
 
       <v-col cols="12" md="6">
         <v-text-field
-          label="Nomor Telepon 1"
           v-model="record.phone"
+          label="Nomor Telepon 1"
           :error-messages="invalidInputMessage.phone"
         ></v-text-field>
       </v-col>
 
       <v-col cols="12" md="6">
         <v-text-field
-          label="Nomor Telepon 2"
           v-model="record.phone_alternative"
+          label="Nomor Telepon 2"
           :error-messages="invalidInputMessage.phone_alternative"
         ></v-text-field>
       </v-col>
     </v-row>
 
     <v-text-field
-      label="Email"
       v-model="record.email"
+      label="Email"
       :error-messages="invalidInputMessage.email"
     ></v-text-field>
 
     <v-text-field
-      label="Alamat"
       v-model="record.address"
+      label="Alamat"
       :error-messages="invalidInputMessage.address"
     ></v-text-field>
 
     <v-text-field
-      label="Kota"
       v-model="record.city"
+      label="Kota"
       :error-messages="invalidInputMessage.city"
     ></v-text-field>
 
     <v-text-field
-      label="Negara"
       v-model="record.country"
+      label="Negara"
       :error-messages="invalidInputMessage.country"
     ></v-text-field>
 
     <v-text-field
-      label="Kode Pos"
       v-model="record.postal_code"
+      label="Kode Pos"
       :error-messages="invalidInputMessage.postal_code"
     ></v-text-field>
 
