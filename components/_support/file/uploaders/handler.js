a7031b58f0146372d65bb0040198de3f9d4b43f1
Andrias Melianus Siwabessy <andriasmelianus@gmail.com>
2021-11-20 00:16:59 +0700

    Add generic handler for file uploader controls.

---
+++ components/_support/file/uploaders/handler.js
@@ -0,0 +1,92 @@
+import { GlobalUploadHandler } from "~/components/_mixins/global-upload-handler";
+export const Handler = {
+  mixins: [GlobalUploadHandler],
+
+  props: {
+    placeHolder: {
+      type: String,
+      default: "Pilih file"
+    },
+
+    accept: {
+      type: String,
+      default: undefined
+    },
+
+    resourceUri: {
+      type: String,
+      default: "/file"
+    },
+
+    /**
+     * Useful for delete operation without sending DELETE request.
+     * Usually used by single image upload, where delete means set the ID to undefined only.
+     */
+    disableDeleteRequest: {
+      type: Boolean,
+      default: false
+    },
+
+    downloadable: {
+      type: Boolean,
+      default: true
+    },
+
+    deletable: {
+      type: Boolean,
+      default: true
+    },
+
+    fieldName: {
+      type: String,
+      default: "file"
+    },
+
+    /**
+     * Card width of the uploaded file.
+     */
+    cardWidth: {
+      type: [String, Number]
+    },
+
+    /**
+     * Card height of the uploaded file.
+     */
+    cardHeight: {
+      type: [String, Number]
+    },
+
+    iconSize: {
+      type: Number,
+      default: 64
+    }
+  },
+
+  data: () => ({
+    description: ""
+  }),
+
+  watch: {
+    /**
+     * Add additional parameter to the upload request.
+     * @param {String} newDescription Newly typed description.
+     * @param {String} oldDescription Previous value of description.
+     */
+    description(newDescription, oldDescription) {
+      this.additionalData[0] = { description: newDescription };
+    }
+  },
+
+  methods: {
+    /**
+     * Transfer event fired from file card to the parent component.
+     * @param {Number|String} idToDelete
+     */
+    handleDeleteButtonClicked(idToDelete) {
+      let vm = this;
+      if (!vm.deletable) {
+        vm.$emit("delete-button-clicked", idToDelete);
+      }
+    }
+  }
+};
