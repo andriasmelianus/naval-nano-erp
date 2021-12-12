e006b975b1790874973e2bd06a15cf09c8a258ad
Andrias Melianus Siwabessy <andriasmelianus@gmail.com>
2021-12-13 01:07:02 +0700

    Add file card dedicated to displaying image.

---
+++ components/_generic/file/cards/image-default.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-card hover :width="width ? width : undefined">
+    <v-img :src="imageSource" :width="width ? width : '100%'" :height="height">
+      <v-app-bar flat color="rgba(0, 0, 0, 0)">
+        <v-spacer></v-spacer>
+
+        <v-btn color="error" @click="handleDeleteButtonClicked">
+          <v-icon>mdi-delete-outline</v-icon>
+        </v-btn>
+      </v-app-bar>
+    </v-img>
+  </v-card>
+</template>
+
+<script>
+import { Handler } from "./handler";
+export default {
+  mixins: [Handler],
+};
+</script>
